fdaa6062e06a4e6e7ba286f2dbe0d58142be27ce
GyunNote <rlaehrbs1968@naver.com>
2024-01-15 17:39:48 +0900

    2024-01-15

---
+++ src/st01/Hello.java
@@ -0,0 +1,23 @@
+package st01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num01 = 130;
+		long num02 = 20;
+		byte num03 = 0;
+		
+		num02 = num01;
+		num03 = (byte)num01;
+		
+		System.out.println("형변환1 int" + num01 + "---> long" + num02);
+		System.out.println(num03);
+		
+		
+	
+		
+		
+	}
+
+}
